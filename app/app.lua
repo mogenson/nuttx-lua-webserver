6eece40924bf1eccbcee4b52580a2019f59a2a10
Michael Mogenson <michael.mogenson@gmail.com>
2022-08-28 11:34:03 -0400

    add css style

--- app/app.lua
+++ app/app.lua
@@ -1,8 +1,7 @@
-local Application = require "atlas.application"
-local Route = require "atlas.route"
-local Response = require "atlas.response"
-
-local leds = io.open("/dev/userleds", "wb")
+local Application = require("atlas.application")
+local Route = require("atlas.route")
+local Response = require("atlas.response")
+local luv = luv or require("luv")
 
 function home()
     local uname = luv.os_uname()
@@ -11,30 +10,34 @@ function home()
     local mem = "Using " .. collectgarbage("count") .. " Kb"
     local links = "Go to <a href=/on>On</a> or <a href=/off>Off</a> to set Led"
 
-    local response = ""
+    local html =
+        "<html><head><link rel=stylesheet href=http://cdn.jsdelivr.net/npm/water.css/out/water.css></head><body>"
+
     for _, content in ipairs({hello, mem, links}) do
-        response = response .. "<p>" .. content .. "</p>"
+        html = html .. "<p>" .. content .. "</p>"
     end
 
-    return response
+    return html .. "</body></html>"
 end
 
 function led(state)
-    leds:ioctl(ioctls.ULEDIOC_SETLED, 0, state)
+    local leds = io.open("/dev/userleds", "wb")
+    if leds then
+        leds:ioctl(ioctls.ULEDIOC_SETLED, 0, state)
+        io.close(leds)
+    end
     return "Led set " .. (state and "On" or "Off")
 end
 
 local controllers = {
-    home = function(request) return Response(home()) end,
     on = function(request) return Response(led(true), "text/plain") end,
-    off = function(request) return Response(led(false), "text/plain") end
+    off = function(request) return Response(led(false), "text/plain") end,
+    home = function(request) return Response(home()) end
 }
 
 local routes = {
-    Route("/", controllers.home), Route("/on", controllers.on),
-    Route("/off", controllers.off)
+    Route("/on", controllers.on), Route("/off", controllers.off),
+    Route("/", controllers.home)
 }
-
 local app = Application(routes)
-
 return {app = app}
